c21566de61ac995a62f044523442cb0205f9f46e
Leif Johansson <leifj@sunet.se>
2019-07-01 14:51:21 +0200

    fetch from a1

--- global/overlay/etc/puppet/modules/thiss/manifests/mdq.pp
+++ global/overlay/etc/puppet/modules/thiss/manifests/mdq.pp
@@ -1,9 +1,15 @@
-class thiss::mdq($version="latest") {
+class thiss::mdq($version="latest", $src=undef) {
    file {"/etc/thiss/metadata.json":
       ensure   => 'present',
       replace  => 'no',
       content  => "[]",
       mode     => '0644'
+   } ->
+   sunet::scriptherder::cronjob { "${name}_fetch_metadata":
+     cmd           => "wget -qO/etc/thiss/metadata.json.new $src && test -s /etc/thiss/metadata.json.new && mv /etc/thiss/metadata.json.new /etc/thiss/metadata.json",
+     minute        => '*/5',
+     ok_criteria   => ['exit_status=0','max_age=48h'],
+     warn_criteria => ['exit_status=1','max_age=50h'],
    } ->
    sunet::docker_run { "thiss_mdq":
       hostname => "${::fqdn}",
