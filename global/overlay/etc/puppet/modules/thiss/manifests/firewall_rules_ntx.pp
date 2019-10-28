8ed8f2452581b280b7b23ed8ca29584c0aeed59a
Maria Haider <mariah@nordu.net>
2019-10-28 13:02:51 +0100

    Chagned two ufw rules

--- global/overlay/etc/puppet/modules/thiss/manifests/firewall_rules_ntx.pp
+++ global/overlay/etc/puppet/modules/thiss/manifests/firewall_rules_ntx.pp
@@ -3,18 +3,18 @@ class thiss::firewall_rules_ntx{
   $md_ntx = hiera_array('md_ntx',[])
   $haproxy_ntx = hiera_array('haproxy_ntx',[])
 
-  #aggregator expose port 80 to md
+  #aggregator expose port 443 to md
   if $::fqdn =~ /^a-[0-9]+\S+\.seamlessaccess\.org$/ {
     sunet::misc::ufw_allow { 'allow_http_aggregator':
       from =>  $md_ntx,
-      port => '80',
+      port => '443',
     }
   }
-  #mdq exposes 443 to haproxy
+  #mdq exposes 80 to haproxy
   if $::fqdn =~ /^md-[0-9]+\S+\.seamlessaccess\.org$/ {
     sunet::misc::ufw_allow { 'allow_https_mdq':
       from => $haproxy_ntx,
-      port => '443',
+      port => '80',
     }
   }
   #haproxy exposes 443 to Internet
