f9d065de97631d5f2de7de2bd18f47b7b025a870
Maria Haider <mariah@nordu.net>
2019-11-05 17:27:30 +0100

    haproxy configuration added
    Also removed the 'docker restart..' part in the script for fetching
    metadata

---
+++ global/overlay/etc/puppet/modules/thiss/manifests/haproxy.pp
@@ -0,0 +1,37 @@
+class thiss::haproxy{
+
+  ensure_resource(sunet::misc::system_user, 'haproxy', {group => 'haproxy' })
+
+  if has_key($::tls_certificates, $::fqdn) and has_key($::tls_certificates[$::fqdn], 'infra_cert') {
+    $infra_cert = $::tls_certificates[$::fqdn]['infra_cert']
+    $infra_key = $::tls_certificates[$::fqdn]['infra_key']
+    ensure_resource(sunet::misc::certbundle, "${::fqdn}_haproxy", {
+      group     => 'haproxy',
+      bundle    => ["cert=${infra_cert}",
+                    "key=${infra_key}",
+                    "out=private/${::fqdn}_haproxy.crt",
+                    ],
+      })
+
+    ensure_resource('file','/opt/haproxy', { ensure => directory } )
+    ensure_resource('file','/opt/haproxy/compose', { ensure => directory } )
+    $servers = hiera_array('md_ntx',[])
+
+    file { '/opt/haproxy/haproxy.cfg':
+      content      => template('thiss/haproxy/haproxy.cfg.erb'),
+      owner        => root,
+      group        => 'haproxy',
+      mode         => '0640'
+    }
+
+  } else {
+    warning("No infra-certificate found for host ${::fqdn} - not configuring simple haproxy")
+  }
+
+  sunet::docker_compose {'haproxy_docker_compose':
+    service_name => 'haproxy_seamlessaccess',
+    description  => 'HAProxy Load Blanacer for mdq',
+    compose_dir  => '/opt/haproxy/compose',
+    content => template('thiss/haproxy/haproxy_seamlessaccess.yml.erb'),
+  }
+}
