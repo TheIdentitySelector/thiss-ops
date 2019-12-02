14669c8b0ae1673716c3363f003f82984ae26ee7
Leif Johansson <leifj@sunet.se>
2019-12-02 14:24:34 +0100

    drop trailing slash

--- global/overlay/etc/puppet/modules/thiss/manifests/pyffd.pp
+++ global/overlay/etc/puppet/modules/thiss/manifests/pyffd.pp
@@ -2,7 +2,7 @@ class thiss::pyffd($pyff_version="latest") {
   $image_tag = "docker.sunet.se/pyff:${pyff_version}"
   file {"/opt/pyff": ensure => directory } ->
   sunet::scriptherder::cronjob { "mirror": 
-    cmd               => "/usr/local/bin/mirror-mdq.sh http://localhost:8080/ /var/www/html/",
+    cmd               => "/usr/local/bin/mirror-mdq.sh http://localhost:8080 /var/www/html/",
     minute            => '*/30',
     ok_criteria       => ['exit_status=0'],
     warn_criteria     => ['max_age=30m']
