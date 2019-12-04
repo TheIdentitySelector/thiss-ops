6eaaa042e9842f05ec3fa749f3524b1ce911b5cd
Leif Johansson <leifj@sunet.se>
2019-12-04 11:30:47 +0100

    move a-1.thiss.io to new pyffd class

--- global/overlay/etc/puppet/modules/thiss/manifests/pyffd.pp
+++ global/overlay/etc/puppet/modules/thiss/manifests/pyffd.pp
@@ -2,20 +2,16 @@ class thiss::pyffd($pyff_version="latest") {
   $image_tag = "docker.sunet.se/pyff:${pyff_version}"
   file {"/opt/pyff": ensure => directory } ->
   sunet::scriptherder::cronjob { "mirror": 
-    cmd               => "/usr/local/bin/mirror-mdq.sh http://localhost:8080 /var/www/html/",
-    minute            => '*/30',
-    ok_criteria       => ['exit_status=0'],
-    warn_criteria     => ['max_age=30m']
+    cmd               => "/bin/true",
+    ensure            => absent
   } ->
   sunet::scriptherder::cronjob { "publish":
     ensure            => absent,
     cmd               => "/bin/true"
   }
+  package { 'lighttpd': ensure => removed } ->
   sunet::pyff {'mdq':
      version           => $pyff_version,
-     ip                => "127.0.0.1",
-     port              => "8080",
      dir               => "/opt/pyff",
-     pound_and_varnish => false
   }
 }
