fa7be35051799b1b26b2a113cb101af2bf73ad91
Leif Johansson <leifj@sunet.se>
2019-12-04 13:20:10 +0100

    fix spelling

--- global/overlay/etc/puppet/modules/thiss/manifests/pyffd.pp
+++ global/overlay/etc/puppet/modules/thiss/manifests/pyffd.pp
@@ -9,7 +9,7 @@ class thiss::pyffd($pyff_version="latest") {
     ensure            => absent,
     cmd               => "/bin/true"
   }
-  package { 'lighttpd': ensure => removed } -> service {'lighttpd': ensure => stoped } ->
+  package { 'lighttpd': ensure => removed } -> service {'lighttpd': ensure => stopped } ->
   sunet::pyffd {'mdq':
      version           => $pyff_version,
      dir               => "/opt/pyff",
