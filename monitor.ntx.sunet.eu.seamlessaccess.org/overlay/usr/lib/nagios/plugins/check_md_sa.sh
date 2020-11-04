9cd353b0a1186c8d5a1715e043355f0f1e621497
Maria Haider <mariah@nordu.net>
2020-11-04 11:36:23 +0100

    added a bash script in nagios for checking metadata age in the URL

---
+++ monitor.ntx.sunet.eu.seamlessaccess.org/overlay/usr/lib/nagios/plugins/check_md_sa.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#
+URL=$1
+
+
+JSONDATE=$(curl -ksL $URL | jq .last_updated 2> /dev/null)
+EXITCODE=$?
+
+if [ $EXITCODE -ne 0 ]; then
+    echo "Could not get date from URL: $URL"
+    exit 2
+fi
+
+JSONDATE=$(echo $JSONDATE | sed s/\"//g)
+
+
+URLDATE=`date -d $JSONDATE +%s`
+EXITCODE=$?
+
+if [ $EXITCODE -ne 0 ]; then
+    echo "Could not parse date:: $JSONDATE"
+    exit 2
+fi
+
+
+TWODAYSAGO=`date -d "-2 days" +%s`
+FIVEDAYSAGO=`date -d "-5 days" +%s`
+
+if [ $FIVEDAYSAGO -ge $URLDATE ]; then
+    echo "Metadata is more then 5 days old!"
+    exit 2
+fi
+
+
+if [ $TWODAYSAGO -ge $URLDATE ]; then
+    echo "Metadata is more then 2 days old!"
+    exit 1
+fi
+
+echo "OK - Metadata is from: $JSONDATE"
+exit 0
