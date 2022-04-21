ff7c4db63a062d20576f3147475818416514980b
Björn Mattsson <bjorn@sunet.se>
2022-04-21 16:02:00 +0200

    Added demo-sp

---
+++ global/overlay/etc/puppet/modules/thiss/manifests/demo_sp.pp
@@ -0,0 +1,20 @@
+class thiss::demo_sp($version='latest')
+{
+  sunet::docker_run { 'always-https':
+    image => 'docker.sunet.se/always-https',
+    ports => ['80:80'],
+    env   => ['ACME_URL=http://acme-c.sunet.se/'],
+  }
+
+  sunet::docker_run { 'demo-sp':
+    hostname            => "${::fqdn}",
+    image               => 'docker.sunet.se/swamid/metadata-sp',
+    imagetag            => $version,
+    env                 => ['SP_METADATAFEED=http://mds.swamid.se/md/swamid-idp-transitive.xml'],
+    volumes             => ['/var/www:/var/www',
+                            '/etc/dehydrated/certs/metadata.lab.swamid.se:/etc/dehydrated',
+                            '/etc/shibboleth/certs:/etc/shibboleth/certs'],
+    ports               => ['443:443'],
+    uid_gid_consistency => false
+  }
+}
