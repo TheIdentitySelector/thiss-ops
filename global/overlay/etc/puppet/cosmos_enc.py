c400bba97d8dae9c422937691ab1c174277fe77d
Fredrik Thulin <fredrik@thulin.net>
2023-02-07 14:21:29 +0100

    remove 'make db'
    The db-file, essentially providing reverse lookup of classes to host
    names, is only used by some Nagios configuration instances and causes
    continuing operational headaches in those ops-repos.
    
    It should be kept/refactored to only apply to the monitoring hosts in
    the cases where it is used, but we don't want any new ops-repos to use
    it hence it should be removed from upstream multiverse.

--- global/overlay/etc/puppet/cosmos_enc.py
+++ global/overlay/etc/puppet/cosmos_enc.py
@@ -1,18 +1,37 @@
 #!/usr/bin/env python3
+#
+# Puppet 'External Node Classifier' to tell puppet what classes to apply to this node.
+#
+# Docs: https://puppet.com/docs/puppet/5.3/nodes_external.html
+#
 
-import sys
-import yaml
 import os
 import re
+import sys
+
+import yaml
+
+rules_path = os.environ.get("COSMOS_RULES_PATH", "/etc/puppet")
 
 node_name = sys.argv[1]
 
-db_file = os.environ.get("COSMOS_ENC_DB","/etc/puppet/cosmos-db.yaml")
-db = dict(classes=dict())
+rules = dict()
+for p in rules_path.split(":"):
+    rules_file = os.path.join(p, "cosmos-rules.yaml")
+    if os.path.exists(rules_file):
+        with open(rules_file) as fd:
+            rules.update(yaml.safe_load(fd))
 
-if os.path.exists(db_file):
-   with open(db_file) as fd:
-      db.update(yaml.load(fd))
+found = False
+classes = dict()
+for reg, cls in rules.items():
+    if re.search(reg, node_name):
+        classes.update(cls)
+        found = True
 
-print(yaml.dump(dict(classes=db['classes'].get(node_name,dict()),parameters=dict(roles=db.get('members',[])))))
+if not found:
+   sys.stderr.write(f"{sys.argv[0]}: {node_name} not found in cosmos-rules.yaml\n")
 
+print("---\n" + yaml.dump(dict(classes=classes)))
+
+sys.exit(0)
