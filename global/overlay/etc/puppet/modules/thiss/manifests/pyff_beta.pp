8d1c547aef276bdb3e645dc0ce03fc923df6ef0a
Erik Bergström <berra@sunet.se>
2023-11-02 15:29:05 +0100

    removed hiera feeds from pyff_beta

--- global/overlay/etc/puppet/modules/thiss/manifests/pyff_beta.pp
+++ global/overlay/etc/puppet/modules/thiss/manifests/pyff_beta.pp
@@ -1,7 +1,6 @@
 class thiss::pyff_beta($pyff_version="thiss",$output="/etc/thiss/metadata.json") {
   $image_tag = "docker.sunet.se/pyff:${pyff_version}"
   $pipeline = hiera("pyff_pipeline_beta")
-  $feeds = hiera("pyff_feeds_beta")
   
   docker::image { "${image_tag}" :
     image   => $image_tag,
