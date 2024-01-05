a236b2a0cb3ba149dae18041dd45f396d9dcc0cc
Maria Haider <mariah@sunet.se>
2024-01-05 14:46:12 +0100

    corrections

--- global/overlay/etc/puppet/modules/thiss/manifests/fleetlock_reboot.pp
+++ global/overlay/etc/puppet/modules/thiss/manifests/fleetlock_reboot.pp
@@ -1,40 +1,37 @@
-class thiss::feetlock_reboot(
+class thiss::fleetlock_reboot(
   String $fleetlock_group,
   String $fleetlock_server,
 ){
 
-  $content = "fleetlock_group ${fleetlock_group}"
-
+  $content = "fleetlock_group=${fleetlock_group}"
   file { '/etc/run-cosmos-fleetlock-conf':
     ensure  => file,
     content => $content,
   }
 
-  $password=safe_hiera('fleetlock_password',''),
+  $password = safe_hiera('fleetlock_password',[])
 
   $content2 = "[${fleetlock_group}]
-                server = ${fleetlock_server}
-                password = ${password}"
+server = ${fleetlock_server}
+password = ${password}"
 
   file { '/etc/sunet-fleetlock/':
     ensure => directory,
   }
-
   -> file { '/etc/sunet-fleetlock/sunet-fleetlock.conf':
       ensure  => file,
       content => $content2,
      }
 
-	$content3 = "#!/bin/bash
-
-              /usr/lib/nagios/plugins/check_http -H localhost -u http://localhost"
-
-  file { '/etc/sunet-machine-healthy/health-checks.d':
+  file { '/etc/sunet-machine-healthy':
     ensure => directory,
   }
-
-  -> file { '/etc/sunet-machine-healthy/health-checks.d/check_web':
+  -> file { '/etc/sunet-machine-healthy/health-checks.d':
+      ensure => directory,
+     }
+  -> file { '/etc/sunet-machine-healthy/health-checks.d/check_web.sh':
       ensure  => file,
-      content => $content3,
+      content => template("thiss/fleetlock_reboot/check_web.sh.erb"),
+      mode    => '0744',
      }
 }
