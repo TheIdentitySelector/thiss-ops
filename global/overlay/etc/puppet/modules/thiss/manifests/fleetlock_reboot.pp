3c0510da6acc795630639a786bcd10427c602718
Maria Haider <mariah@sunet.se>
2024-01-05 13:57:55 +0100

    reboot with fleetlock

---
+++ global/overlay/etc/puppet/modules/thiss/manifests/fleetlock_reboot.pp
@@ -0,0 +1,40 @@
+class thiss::feetlock_reboot(
+  String $fleetlock_group,
+  String $fleetlock_server,
+){
+
+  $content = "fleetlock_group ${fleetlock_group}"
+
+  file { '/etc/run-cosmos-fleetlock-conf':
+    ensure  => file,
+    content => $content,
+  }
+
+  $password=safe_hiera('fleetlock_password',''),
+
+  $content2 = "[${fleetlock_group}]
+                server = ${fleetlock_server}
+                password = ${password}"
+
+  file { '/etc/sunet-fleetlock/':
+    ensure => directory,
+  }
+
+  -> file { '/etc/sunet-fleetlock/sunet-fleetlock.conf':
+      ensure  => file,
+      content => $content2,
+     }
+
+	$content3 = "#!/bin/bash
+
+              /usr/lib/nagios/plugins/check_http -H localhost -u http://localhost"
+
+  file { '/etc/sunet-machine-healthy/health-checks.d':
+    ensure => directory,
+  }
+
+  -> file { '/etc/sunet-machine-healthy/health-checks.d/check_web':
+      ensure  => file,
+      content => $content3,
+     }
+}
