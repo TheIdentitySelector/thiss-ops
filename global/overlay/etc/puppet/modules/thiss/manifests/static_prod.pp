3545d6ef27e4f3fd29ed569dd0c925014d0166f2
Maria Haider <mariah@sunet.se>
2025-02-07 16:45:07 +0100

    changing to docker compose to match ubuntu 24 setup
    ref: SA-131

--- global/overlay/etc/puppet/modules/thiss/manifests/static_prod.pp
+++ global/overlay/etc/puppet/modules/thiss/manifests/static_prod.pp
@@ -6,55 +6,71 @@ class thiss::static_prod($ds_version="latest",
                       $mdq_hostport=undef,
                       $cache_control=undef) {
 
-   $final_mdq_search_url = $mdq_search_url ? {
+  $final_mdq_search_url = $mdq_search_url ? {
     undef   => chop($base_url)/entities,
     default => $mdq_search_url,
-   }
-   $component_url=chop($base_url)
-   $persistence_url=chop($base_url)
-   $whitelist = hiera ('whitelist')
+  }
+  $component_url=chop($base_url)
+  $persistence_url=chop($base_url)
+  $whitelist = hiera ('whitelist')
 
-   sunet::snippets::somaxconn { "ds_nginx": maxconn => 4096 }
-   if $mdq_hostport {
-    $mdq_search_url=chop($base_url)
-    sunet::docker_run { "thiss_js":
-      hostname => "${::fqdn}",
-      image    => "docker.sunet.se/thiss-js",
-      imagetag => $ds_version,
-      env      => ["BASE_URL=$base_url",
-                   "MDQ_URL=$final_mdq_search_url",
-                   "SEARCH_URL=$final_mdq_search_url",
-                   "STORAGE_DOMAIN=$domain",
-                   "LOGLEVEL=warn",
-                   "DEFAULT_CONTEXT=$context",
-                   "MDQ_HOSTPORT=$mdq_hostport",
-                   "COMPONENT_URL=$component_url/cta/",
-                   "PERSISTENCE_URL=$persistence_url/ps/",
-                   "CACHE_CONTROL=\"$cache_control\"",
-                   "WHITELIST=$whitelist"],
-      volumes  => ["/etc/ssl:/etc/ssl"],
-      ports    => ["80:80"],
-      extra_parameters => ["--log-driver=syslog"]
-    }
-   }
-  else {
-    sunet::docker_run { "thiss_js":
-      hostname => "${::fqdn}",
-      image    => "docker.sunet.se/thiss-js",
-      imagetag => $ds_version,
-      env      => ["BASE_URL=$base_url",
-                   "MDQ_URL=$final_mdq_search_url",
-                   "SEARCH_URL=$final_mdq_search_url",
-                   "STORAGE_DOMAIN=$domain",
-                   "LOGLEVEL=warn",
-                   "DEFAULT_CONTEXT=$context",
-                   "COMPONENT_URL=$component_url/cta/",
-                   "PERSISTENCE_URL=$persistence_url/ps/",
-                   "CACHE_CONTROL=\"$cache_control\"",
-                   "WHITELIST=$whitelist"],
-      volumes  => ["/etc/ssl:/etc/ssl"],
-      ports    => ["80:80"],
-      extra_parameters => ["--log-driver=syslog"]
+  sunet::snippets::somaxconn { "ds_nginx": maxconn => 4096 }
+
+  if $::facts['dockerhost2'] == 'yes' {
+
+    ensure_resource('file','/opt/static', { ensure => directory } )
+    ensure_resource('file','/opt/static/compose', { ensure => directory } )
+
+    sunet::docker_compose {'thiss-js':
+      service_name => 'thiss_js',
+      description  => 'SA identity selector software',
+      compose_dir  => '/opt/static/compose',
+      content => template('thiss/static/thiss-js.yml.erb'),
     }
   }
+  else {
+    if $mdq_hostport {
+      $mdq_search_url=chop($base_url) #not in docker-compose section, unclear why it is here
+      sunet::docker_run { "thiss_js":
+        hostname => $facts['networking']['fqdn'],
+        image    => "docker.sunet.se/thiss-js",
+        imagetag => $ds_version,
+        env      => ["BASE_URL=$base_url",
+                     "MDQ_URL=$final_mdq_search_url",
+                     "SEARCH_URL=$final_mdq_search_url",
+                     "STORAGE_DOMAIN=$domain",
+                     "LOGLEVEL=warn",
+                     "DEFAULT_CONTEXT=$context",
+                     "MDQ_HOSTPORT=$mdq_hostport",
+                     "COMPONENT_URL=$component_url/cta/",
+                     "PERSISTENCE_URL=$persistence_url/ps/",
+                     "CACHE_CONTROL=\"$cache_control\"",
+                     "WHITELIST=$whitelist"],
+        volumes  => ["/etc/ssl:/etc/ssl"],
+        ports    => ["80:80"],
+        extra_parameters => ["--log-driver=syslog"]
+    }
+  }
+    else {
+      sunet::docker_run { "thiss_js":
+        hostname => $facts['networking']['fqdn'],
+        image    => "docker.sunet.se/thiss-js",
+        imagetag => $ds_version,
+        env      => ["BASE_URL=$base_url",
+                     "MDQ_URL=$final_mdq_search_url",
+                     "SEARCH_URL=$final_mdq_search_url",
+                     "STORAGE_DOMAIN=$domain",
+                     "LOGLEVEL=warn",
+                     "DEFAULT_CONTEXT=$context",
+                     "COMPONENT_URL=$component_url/cta/",
+                     "PERSISTENCE_URL=$persistence_url/ps/",
+                     "CACHE_CONTROL=\"$cache_control\"",
+                     "WHITELIST=$whitelist"],
+        volumes  => ["/etc/ssl:/etc/ssl"],
+        ports    => ["80:80"],
+        extra_parameters => ["--log-driver=syslog"]
+      }
+    }
+  }
+
 }
