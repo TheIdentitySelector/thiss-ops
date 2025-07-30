5c83740b9d169cc47acd2765d6475522cc446847
Maria Haider <mariah@sunet.se>
2025-07-30 16:57:12 +0200

    reinstalling meta.ntx.sunet.eu.seamlessaccess.org

---
+++ global/overlay/etc/puppet/modules/thiss/manifests/pyff_prod.pp
@@ -0,0 +1,29 @@
+class thiss::pyff_prod($pyff_version="thiss",$output="/etc/thiss/metadata.json",$output_trust="/etc/thiss/metadata_sp.json") {
+  $image_tag = "docker.sunet.se/pyff:${pyff_version}"
+  $pipeline = hiera("pyff_pipeline_prod")
+
+  package {'xmlsec1': ensure => present}
+
+  $image_find = "docker images |grep docker.sunet.se/pyff |grep ${image_tag}"
+
+  exec { 'pull_docker_images':
+    command      => "docker pull ${image_tag}",
+    unless       => $image_find,
+    require      => Class['sunet::dockerhost2'],
+  } ->
+
+  file {["/etc/thiss","/opt/pyff","/opt/pyff/metadata"]: ensure => directory } ->
+  file {"/usr/local/sbin/run-pyff":
+     content => template("thiss/pyff/run-pyff_prod.erb"),
+     mode    => '0755'
+  } ->
+  file {"/opt/pyff/mdx.fd":
+     content => inline_template("<%= @pipeline.to_yaml %>\n")
+  }
+  sunet::scriptherder::cronjob { "publish":
+    cmd               => "/usr/local/sbin/run-pyff /opt/pyff/mdx.fd $output $output_trust",
+    minute            => '*/30',
+    ok_criteria       => ['exit_status=0'],
+    warn_criteria     => ['max_age=30m']
+  }
+}
