1e09a24d6fbad303e361ea318edf10ed8ca064b4
Maria Haider <mariah@sunet.se>
2025-12-02 21:00:11 +0100

    adding enrique's IP

--- global/overlay/etc/puppet/modules/thiss/manifests/log.pp
+++ global/overlay/etc/puppet/modules/thiss/manifests/log.pp
@@ -1,9 +1,14 @@
 class thiss::log{
 
   $enrique_ssh_key = lookup ('enrique_ssh_key')
+  $enrique_ip = lookup ('enrique_ip')
 
   sunet::rrsync { '/var/log/':
     ssh_key_type       => 'ssh-ed25519',
     ssh_key            => $enrique_ssh_key,
+  }
+
+  sunet::nftables::rule { 'allow_rsync':
+    rule => "add rule inet filter input ip saddr {${enrique_ip}} tcp dport 22 counter accept comment \"allow-rsync-from-ni-host\""
   }
 }
