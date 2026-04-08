345e6a2aa8db00ac0579744af86261d3d417af61
Rasmus Thorslund <thorslund@sunet.se>
2026-04-08 13:33:24 +0200

    added dockerhost2 conf for demo_sp

--- global/overlay/etc/puppet/modules/thiss/manifests/demo_sp.pp
+++ global/overlay/etc/puppet/modules/thiss/manifests/demo_sp.pp
@@ -1,25 +1,15 @@
 class thiss::demo_sp($version='stable')
 {
-  sunet::docker_run { 'always-https':
-    image => 'docker.sunet.se/always-https',
-    ports => ['80:80'],
-    env   => ['ACME_URL=http://acme-c.sunet.se/'],
+
+  ensure_resource('file','/opt/demo_sp', { ensure => directory } )
+
+  sunet::docker_compose {'demo-sp':
+    service_name => 'demo-sp',
+    description  => 'SA demo sp',
+    compose_dir  => '/opt/demo_sp/compose',
+    content => template('thiss/demo_sp/demo_sp.yml.erb'),
   }
 
-  sunet::docker_run { 'demo-sp':
-    hostname            => "$facts['networking']['fqdn']",
-    image               => 'docker.sunet.se/swamid/metadata-sp',
-    imagetag            => $version,
-    #env                 => ['SP_METADATAFEED=http://mds.swamid.se/md/swamid-idp-transitive.xml'],
-    volumes             => ['/var/www:/var/www:ro',
-                            "/etc/dehydrated/certs/$facts['networking']['fqdn']:/etc/dehydrated:ro",
-                            '/etc/shibboleth/certs:/etc/shibboleth/certs',
-                            '/etc/shibboleth/shibboleth2.xml:/etc/shibboleth/shibboleth2.xml:ro',
-                            '/etc/apache2/start.sh:/start.sh',
-                            '/etc/apache2/sites-available/default-ssl.conf:/etc/apache2/sites-available/default-ssl.conf:ro'],
-    ports               => ['443:443'],
-    uid_gid_consistency => false
-  }
   vcsrepo { '/var/www/demo':
     ensure   => latest,
     provider => git,
