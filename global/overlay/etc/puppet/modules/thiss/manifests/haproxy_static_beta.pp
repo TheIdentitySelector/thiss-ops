a132cda52dedad71c68986c525f53f9302baf145
Maria Haider <mariah@sunet.se>
2026-08-19 17:39:58 +0200

    adding auto cert renewal

--- global/overlay/etc/puppet/modules/thiss/manifests/haproxy_static_beta.pp
+++ global/overlay/etc/puppet/modules/thiss/manifests/haproxy_static_beta.pp
@@ -45,4 +45,10 @@ class thiss::haproxy_static_beta($location=undef,$image_tag=undef,$image_name='d
     compose_dir  => '/opt/haproxy/compose',
     content => template('thiss/haproxy/haproxy_seamlessaccess_static.yml.erb'),
   }
+
+   file { '/etc/letsencrypt/renewal-hooks/post/certbot-acmed-renew-post-hook':
+      ensure  => file,
+      mode    => '0700',
+      content => "systemctl restart sunet-haproxy_seamlessaccess.service",
+   }
 }
