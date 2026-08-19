acb7d03542ba35f07e2ef6b3e046b67fbe858fef
Maria Haider <mariah@sunet.se>
2026-08-19 17:13:11 +0200

    create haproxy cert using new infra certs

---
+++ global/overlay/etc/puppet/modules/thiss/manifests/haproxy_static_beta.pp
@@ -0,0 +1,48 @@
+class thiss::haproxy_static($location=undef,$image_tag=undef){
+
+  ensure_resource(sunet::misc::system_user, 'haproxy', {group => 'haproxy' })
+
+  $infra_cert = $::tls_certificates[$facts['networking']['fqdn']]['certbot_fullchain']
+  $infra_key = $::tls_certificates[$facts['networking']['fqdn']]['certbot_key']
+  ensure_resource(sunet::misc::certbundle, "${facts['networking']['fqdn']}_haproxy", {
+    group  => 'haproxy',
+    bundle => ["cert=${infra_cert}",
+               "key=${infra_key}",
+               "out=/etc/ssl/private/${facts['networking']['fqdn']}_haproxy.crt",
+               ],
+  })
+
+    ensure_resource('file','/opt/haproxy', { ensure => directory } )
+    ensure_resource('file','/opt/haproxy/compose', { ensure => directory } )
+    $servers = hiera_hash("static_${location}")
+
+    file { '/opt/haproxy/haproxy.cfg':
+      content      => template('thiss/haproxy/haproxy_static.cfg.erb'),
+      owner        => root,
+      group        => 'haproxy',
+      mode         => '0640'
+    }
+
+  sunet::nftables::docker_expose { 'haproxy' :
+    allow_clients => 'any',
+    port          => '443',
+    iif           => "${interface_default}",
+  }
+
+  if $facts['networking']['fqdn'] =~ /^static(\.\S+\.|\.)thiss\.io$/ {
+    $nagios_ip_v4 = hiera_array('nagios_ip_v4',[])
+    $sunet_vpn = hiera_array('sunet_vpn',[])
+    sunet::nftables::docker_expose { 'haproxy-stats' :
+      allow_clients => $nagios_ip_v4 + $sunet_vpn,
+      port          => '8404',
+      iif           => "${interface_default}",
+    }
+  }
+
+  sunet::docker_compose {'haproxy_docker_compose':
+    service_name => 'haproxy_seamlessaccess',
+    description  => 'HAProxy Load Blanacer for thiss-js',
+    compose_dir  => '/opt/haproxy/compose',
+    content => template('thiss/haproxy/haproxy_seamlessaccess_static.yml.erb'),
+  }
+}
